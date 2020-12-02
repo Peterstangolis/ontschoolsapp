d86343b0867611d3cc6dcc4d9cb2c54643ac02a9
Peter Stangolis <pstangolis@gmail.com>
2020-12-02 16:35:47 -0500

    initial

---
+++ app.py
@@ -0,0 +1,198 @@
+## Python - Ontario Schools COVID-19 Data set App
+
+# Required Libraries:
+
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
+import pandas as pd
+from datetime import datetime
+
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+from dash.dependencies import Input, Output
+
+# Import the Data sets:
+
+### Summary of COVID-19 Cases in Ontario Schools ***
+
+url = "https://data.ontario.ca/dataset/b1fef838-8784-4338-8ef9-ae7cfd405b41/resource/7fbdbb48-d074-45d9-93cb-f7de58950418/download/schoolcovidsummary.csv"
+df_sum = pd.read_csv(url)
+
+## Change the 'reported_date' column to a datetime object and drop 'collected_date'
+df_sum["reported_date"] = pd.to_datetime(df_sum["reported_date"])
+df_sum.drop("collected_date", axis=1, inplace=True)
+
+
+### Ontario Schols with Active Case Data set
+url = "https://data.ontario.ca/dataset/b1fef838-8784-4338-8ef9-ae7cfd405b41/resource/8b6d22e2-7065-4b0f-966f-02640be366f2/download/schoolsactivecovid.csv"
+df_active = pd.read_csv(url)
+df_active["reported_date"] = pd.to_datetime(df_active["reported_date"])
+
+## Drop collected_date from Active data set
+df_active.drop("collected_date", axis=1, inplace=True)
+
+## Filter the Active Cases Data set for the last_reported date
+df_active_now = df_active[df_active.reported_date == max(df_active.reported_date)]
+
+
+## Group the active_now data set by municipality
+df_municiaplity_now = df_active_now.groupby("municipality")['total_confirmed_cases'].sum().reset_index().sort_values(by=["total_confirmed_cases"], ascending=False).head(30)
+
+
+## Set the index of the summary data set to the reported_date column
+## Average the cases per week: Week Starting Sunday
+datetime_index = pd.DatetimeIndex(df_sum.reported_date.values)
+df_weekly = df_sum.set_index(datetime_index)
+df_weekly.drop('reported_date', axis=1, inplace = True)
+
+## Add week number to weekly data set
+df_weekly['week_num'] = df_weekly.index.isocalendar().week
+
+## Ontario Schools COVID-19 Cases Summary data set - Weekly Average
+df_weekly = df_weekly.new_total_school_related_cases.resample("W").mean().round()
+df_weekly = df_weekly.reset_index()
+
+### FINDING THE NEW CASES TOTALS OF THE DAY FOR STUDENTS, STAFF and TOTAL ###
+# Latest Total School Case Number
+value_t  = df_sum.loc[df_sum.index[-1], 'new_total_school_related_cases']
+
+# Previous Day School Total
+reference_t = df_sum.loc[df_sum.index[-2], 'new_total_school_related_cases']
+# Latest Total School Case Number
+value_student  = df_sum.loc[df_sum.index[-1], 'new_school_related_student_cases']
+
+# Previous Day School Total
+reference_student = df_sum.loc[df_sum.index[-2], 'new_school_related_student_cases']
+
+# Latest Total School Case Number
+value_staff  = df_sum.loc[df_sum.index[-1], 'new_school_related_staff_cases']
+
+# Previous Day School Total
+reference_staff = df_sum.loc[df_sum.index[-2], 'new_school_related_staff_cases']
+
+
+#### CREATE THE DASHBOARD ####
+fig = make_subplots(
+        rows = 5, cols = 6,
+        specs = [
+                    [{"type" : "scatter", "rowspan": 3, "colspan" : 3}, None, None, {"type": "indicator"},{"type": "indicator"},{"type": "indicator"} ],
+                    [  None, None, None,                             {"type" : "indicator"},  None, None],
+                    [  None, None, None,                             {"type": "indicator"}, {"type": "indicator", "rowspan" : 1, "colspan": 2}, None],
+                    [{"type" : "bar", "rowspan": 2, "colspan" : 6},  None, None, None, None, None],
+                    [  None, None, None, None, None, None],
+                ]
+)
+
+last_reported_date = max(df_sum.reported_date)
+
+fig.add_trace(
+       go.Scatter(
+        x = df_sum['reported_date'],
+        y = df_sum['cumulative_school_related_cases']),
+        row = 1, col = 1
+     )
+
+fig.update_layout(
+        title = f"Cumulative COVID-19 Cases in Students, Staff & Total <br> Report Updated: {last_reported_date}",
+)
+
+fig.add_trace(
+    go.Indicator(
+        value = value_t,
+        delta = {'reference': reference_t},
+        mode = "number+delta",
+        title = {"text": "New Total Cases"},
+        domain = {'row': 0, 'column': 0}
+    ),
+    row = 1, col = 4
+)
+
+fig.add_trace(
+    go.Indicator(
+        value = value_student,
+        delta = {'reference': reference_student, 'relative': False},
+        mode = "number+delta",
+        title = {"text" :"New Student Cases"},
+        domain = {'row': 0, 'column': 2}),
+     row = 1, col = 5
+)
+
+fig.add_trace(
+    go.Indicator(
+        mode = "number+delta",
+        value = value_staff,
+        delta = {"reference": reference_staff},
+        title = {"text" :"New Staff Cases"},
+        domain = {'row': 0, 'column': 1}),
+    row = 1, col = 6
+)
+
+schools_w_cases = df_sum.current_schools_w_cases[df_sum.reported_date == max(df_sum.reported_date)].values[0]
+
+
+fig.add_trace(
+    go.Indicator(
+        mode = 'number',
+        value = schools_w_cases,
+        title = "Schools with Cases"),
+    row = 2, col = 4
+)
+
+value = df_sum.current_schools_closed[df_sum.reported_date == max(df_sum.reported_date)].values[0]
+
+fig.add_trace(
+    go.Indicator(
+        mode = 'number',
+        value = value,
+        title = "Schools Closed"),
+    row = 3, col = 4
+)
+
+fig.add_trace(
+    go.Indicator(
+        mode = "gauge+number",
+        value = df_sum.reported_date.count(),
+        title = {'text': "<span style='font-size:0.8em'> Schools Days Completed vs <br> Days in School Year</span>"},
+        gauge = {
+            'axis' : {'visible' :True}
+        },
+        gauge_axis = {
+            'range' : [0, 195]}
+    ),
+    row = 3, col = 5
+)
+
+
+colors = ['lightslategrey',] * len(df_municiaplity_now)
+colors[0] = 'crimson'
+
+fig.add_trace(
+    go.Bar(y = df_municiaplity_now['total_confirmed_cases'], x = df_municiaplity_now['municipality'],
+           texttemplate = df_municiaplity_now["total_confirmed_cases"],
+          marker_color= colors),
+    row = 4, col = 1
+)
+
+fig.update_layout(
+    template = "seaborn",
+    title = f"Ontario Schools COVID-19 Cases as of: <b>{max(df_active.reported_date).date()}",
+    showlegend = False
+    ,
+)
+
+
+app = dash.Dash(__name__)
+server = app.server
+
+app.layout = html.Div(style = {'textAlign': 'Center'}, children = [
+
+    dcc.Graph(
+        style = {'height':"100vh"},
+        figure = fig,
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
